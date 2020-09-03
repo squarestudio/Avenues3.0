f05519469abdc7fd7aa5821f9abdca77c1ae9ca3
Dmitriy Karpov <karpovsystems@gmail.com>
2020-09-03 11:59:26 +0300

    fix time filter

--- src/common/scripts/filters/time.js
+++ src/common/scripts/filters/time.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 
 Vue.filter('time', value => {
     value = value || 0;
-    return new Date(value * 1000).toISOString().substr(11, 8);
+    return new Date(value * 1000).toISOString().substr(14, 5);
 })
